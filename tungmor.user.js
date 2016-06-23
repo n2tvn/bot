277e8040540d8ccdafccef1ddad7ac664c20aa44
n2tvn <n2t.services@gmail.com>
2016-06-23 19:28:44 +0700

    Add files via upload

---
+++ tungmor.user.js
@@ -0,0 +1,14 @@
+﻿// ==UserScript==
+// @name         TungMor Bot
+// @namespace    http://slither.io/
+// @version      1.1.11 By https://www.facebook.com/n2t.1st (Tùng Mor)
+// @description  TungMor Bot
+// @author       Tùng Mor
+// @match        http://slither.io/
+// @require		   http://phongcach.pro/btree-complete.js
+// @require		   http://phongcach.pro/bot.astar.js
+// @require		   http://phongcach.pro/bot.gridsystem.js
+// @require		   http://phongcach.pro/bot.behaviors.js
+// @require		   http://phongcach.pro/bot.user.11.js
+// @grant        none
+// ==/UserScript==
